95b2abcea4cbd55a6233e5789a878692fdc7075d
Alisonais <alisonais@ymail.com>
2023-09-28 00:16:17 -0300

    feat: cont criação do componente servicos

--- src/components/main/servicos/index.jsx
+++ src/components/main/servicos/index.jsx
@@ -3,11 +3,11 @@ import Card from "./card";
 
 export default function Servicos() {
   return (
-    <section 
-    className=" py-8 px-4 flex flex-col items-center gap-4 " 
-    style={{
-      background: `${backgroundlinearServicos}`
-    }}
+    <section
+      className=" py-8 px-4 flex flex-col items-center gap-4 "
+      style={{
+        background: `${backgroundlinearServicos}`
+      }}
     >
 
       <h2 className=" font-bold text-[40px] text-center " >
@@ -15,14 +15,28 @@ export default function Servicos() {
       </h2>
 
       <p className=" text-center md:p-8 md:px-20  " >
-      Costa Barber Shop é uma ótima opção para homens que buscam um lugar para cuidar do seu visual. A barbearia oferece uma variedade de serviços de qualidade, prestados por um profissional experiente e qualificado.
+        Costa Barber Shop é uma ótima opção para homens que buscam um lugar para cuidar do seu visual. A barbearia oferece uma variedade de serviços de qualidade, prestados por um profissional experiente e qualifica.
       </p>
 
-      <Card 
-        img={backgroundCta}
-        title="Corte de cabelo"
-        price={45}
-      />
+      <div>
+        <Card
+          img={backgroundCta}
+          title="Corte de cabelo"
+          price={45}
+        />
+
+        <Card
+          img={backgroundCta}
+          title="Corte de cabelo"
+          price={45}
+        />
+
+        <Card
+          img={backgroundCta}
+          title="Corte de cabelo"
+          price={45}
+        />
+      </div>
 
     </section>
   );
