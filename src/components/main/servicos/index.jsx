cb030feb5ce1834ad70208fa9dfe1c43973f537a
Alisonais <alisonais@ymail.com>
2023-09-27 21:22:47 -0300

    configurações de deply

--- src/components/main/servicos/index.jsx
+++ src/components/main/servicos/index.jsx
@@ -3,11 +3,11 @@ import Card from "./card";
 
 export default function Servicos() {
   return (
-    <section 
-    className=" py-8 px-4 flex flex-col items-center gap-4 " 
-    style={{
-      background: `${backgroundlinearServicos}`
-    }}
+    <section
+      className=" py-8 px-4 flex flex-col items-center gap-4 "
+      style={{
+        background: `${backgroundlinearServicos}`
+      }}
     >
 
       <h2 className=" font-bold text-[40px] text-center " >
@@ -18,11 +18,25 @@ export default function Servicos() {
         Lorem ipsum dolor sit amet consectetur. Etiam mus vitae lectus proin pellentesque mattis iaculis. In viverra accumsan sagittis tempus duis a. Sagittis mollis a eu urna eget id.
       </p>
 
-      <Card 
-        img={backgroundCta}
-        title="Corte de cabelo"
-        price={45}
-      />
+      <div className=" w-full flex flex-col gap-4 md:flex-wrap md:flex-row justify-center " >
+        <Card
+          img={backgroundCta}
+          title="CABELO"
+          price={25}
+        />
+
+        <Card
+          img={backgroundCta}
+          title="BARBA"
+          price={20}
+        />
+
+        <Card
+          img={backgroundCta}
+          title={"CABELO E BARBA"}
+          price={110}
+        />
+      </div>
 
     </section>
   );
