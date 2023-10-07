bacad1a396c8c5f1b3486d3873ce508d74b5aa30
Alisonais <alisonais@ymail.com>
2023-10-06 23:58:19 -0300

    feat: adicionando animação

--- src/components/main/cta/index.jsx
+++ src/components/main/cta/index.jsx
@@ -1,12 +1,20 @@
+'use client'
 import Buttom from "@/components/Button";
 import WhatsApp from "../../../../public/whatsapp logo";
 import Instagram from "../../../../public/instagram logo";
 import { backgroundCta, backgroundLinearGradientCta } from '../../utils/imageLinks'
 import Logo from "@/components/utils/logos/LogoCostaBarbearia";
+import { motion } from 'framer-motion';
 
 export default function Cta() {
+  const spring = {
+    type: "spring",
+    damping: 10,
+    stiffness: 100
+  }
   return (
     <section
+
       style={{
         backgroundImage: `url(${backgroundCta})`,
         backgroundSize: 'cover',
@@ -14,49 +22,66 @@ export default function Cta() {
       }}
       className="h-full"
     >
+
       <div
-        className=" h-full w-full text-white flex flex-col items-center justify-evenly p-4 md:p-8 lg:p-16 xl:p-20 "
         style={{ background: `${backgroundLinearGradientCta}` }}
       >
-        <div>
-
-          <Logo />
-
-          <h1
-            className="text-[22px] p-8 font-bold text-center lg:text-[32px] xl:text-[40px] xl: "
-          >
-            Elegância, Ousadia e Barba Alinhada
-          </h1>
-        </div>
-
-        <div>
-          <h2 className=" text-[18px] text-center lg:text-[22px] xl:text-[32px] xl:pb-8 ">
-            Horário de funcionamento: <br /> 08:00h às 19:00h de terça à quarta. <br />
-            08:00h às 21:00h de quinta à sabado.
-          </h2>
-        </div>
-
-        <Buttom content="Agende Agora" />
-
-        <div>
-          <h2 className=" text-[18px] text-center p-5 lg:text-[22px] xl:text-[32px] xl:pb-8 ">
-            conheça nosso instagram, e se tiver alguma dúvida chama no whatsApp
-          </h2>
-        </div>
-
-        <div
-          className="flex gap-8 "
+        <motion.div
+          initial={{
+            display: 'relative',
+            y: '-900px'
+          }}
+          animate={{
+            display: 'flex',
+            y: '0px'
+          }}
+          transition={spring}
+          className=" h-full w-full text-white flex flex-col items-center justify-evenly p-4 md:p-8 lg:p-16 xl:p-20 "
         >
-          <div className="h-6 w-6 md:h-10 md:w-10 lg:h-14 lg:w-14 hover:scale-110 transition-all ease-in-out duration-500 ">
-            <Instagram />
-          </div>
-          <div className="h-6 w-6 md:h-10 md:w-10 lg:h-14 lg:w-14 hover:scale-110 transition-all ease-in-out duration-500 ">
-            <WhatsApp />
+          <div>
+
+            <Logo />
+
+            <h1
+              className="text-[22px] p-8 font-bold text-center lg:text-[32px] xl:text-[40px] xl: "
+            >
+              Elegância, Ousadia e Barba Alinhada
+            </h1>
           </div>
 
-        </div>
+          <div>
+            <h2 className=" text-[18px] text-center lg:text-[22px] xl:text-[32px] xl:pb-8 ">
+              Horário de funcionamento: <br /> 08:00h às 19:00h de terça à quarta. <br />
+              08:00h às 21:00h de quinta à sabado.
+            </h2>
+          </div>
+
+          <Buttom content="Agende Agora" />
+
+          <div>
+            <h2 className=" text-[18px] text-center p-5 lg:text-[22px] xl:text-[32px] xl:pb-8 ">
+              conheça nosso instagram, e se tiver alguma dúvida chama no whatsApp
+            </h2>
+          </div>
+
+          <div
+            className="flex gap-8 "
+          >
+            <div className="h-6 w-6 md:h-10 md:w-10 lg:h-14 lg:w-14 hover:scale-110 transition-all ease-in-out duration-500 ">
+              <Instagram />
+            </div>
+            <div className="h-6 w-6 md:h-10 md:w-10 lg:h-14 lg:w-14 hover:scale-110 transition-all ease-in-out duration-500 ">
+              <WhatsApp />
+            </div>
+
+          </div>
+        </motion.div>
       </div>
 
     </section>
   );
 };
+
+
+
+// className=" h-full w-full text-white flex flex-col items-center justify-evenly p-4 md:p-8 lg:p-16 xl:p-20 "
