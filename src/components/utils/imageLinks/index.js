99732daa14d33ad2d2c6df4e9204abc5f7a71436
Alisonais <alisonais@ymail.com>
2023-10-20 00:31:10 -0300

    refactor: Otimizando imagens

--- src/components/utils/imageLinks/index.js
+++ src/components/utils/imageLinks/index.js
@@ -1,4 +1,5 @@
 import imagem1 from "../../../../public/imagem1.webp";
+import imagem2 from "../../../../public/imagem2.webp";
 import imagem3 from "../../../../public/imagem3.webp";
 import imagem4 from "../../../../public/imagem4.webp";
 import imagem5 from "../../../../public/imagem5.webp";
@@ -10,17 +11,17 @@ export const backgroundCta = imagemBackground;
 
 export const imagem1corte = imagem1
 
-export const imagem2corte = imagem3
+export const imagem2corte = imagem2
 
-export const imagem3corte = imagem4
+export const imagem3corte = imagem3
 
-export const imagem4corte = imagem5
+export const imagem4corte = imagem4
 
-export const imagem5corte = imagem6
+export const imagem5corte = imagem5
 
-export const imagem6corte = imagem7
+export const imagem6corte = imagem6
 
-export const imagem7corte = imagem1
+export const imagem7corte = imagem7
 
 export const backgroundLinearGradientCta = 'linear-gradient(180deg, #0D121E 0%, rgba(13, 18, 30, 0.51) 25%, rgba(13, 18, 30, 0.32) 51.56%, rgba(13, 18, 30, 0.51) 76.04%, #0D121E 100%)';
 
